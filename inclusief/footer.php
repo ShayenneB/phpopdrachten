6f82a1e0b634f2f3bf0ca00ff574bbc3a073142d
Shayenne <shayenne.boll@edu-kw1c.nl>
2020-02-23 15:14:08 +0100

    :D

--- inclusief/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("includes/variabelen.php");
-
-    // De footer via echo getoond aan de gebruiker
-    echo("<div class='footer'>");
-    echo("&copy; $year ");
-    echo("$name</div>");
-?>
--- inclusief/header.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo("<header>");
-echo("<h1><uitwerking van PHP-opdrachten</h1>");
-echo("</header>");
-
-?>
