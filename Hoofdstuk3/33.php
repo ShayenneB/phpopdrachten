ee8010c0a2b7674b173e45876d0956bf3652f597
Shayenne <shayenne.boll@edu-kw1c.nl>
2020-02-23 15:29:59 +0100

    Opdrachten af

---
+++ Hoofdstuk3/33.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+    <html>
+    <head>
+        <meta name="description" content=" ">
+        <meta name="keywords" content=" ">
+        <meta charset="UTF-8">
+        <link href="/phpopdrachten/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <?php
+    include "../Includes/header.php";
+    ?>
+            <a href="/phpopdrachten/index.php">Terug</a>
+    <?php
+    include "../Includes/footer.php";
+    ?>
+    </body>
+    </html>
